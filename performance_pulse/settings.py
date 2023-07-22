dba2a106b3a00e737f28aa7196213328d64254bf
LazyCiao <123471880+LazyCiao@users.noreply.github.com>
2023-07-22 17:54:31 +0200

    Add files via upload

--- performance_pulse/settings.py
+++ performance_pulse/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'financial_pulse.apps.FinancialPulseConfig',
+    'crypto_pulse.apps.CryptoPulseConfig',
     'crispy_forms',
     'crispy_bootstrap4',
 ]
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'performance_pulse.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'performance_pulse', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
